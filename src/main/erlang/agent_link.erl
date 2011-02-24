5406ff1abdc4b78e66d18bd9c5127eb82f0e4bcf
Gregory Haskins <ghaskins@novell.com>
2011-02-24 09:26:46 -0500

    Add get_state()
    Signed-off-by: Gregory Haskins <ghaskins@novell.com>

--- src/main/erlang/agent_link.erl
+++ src/main/erlang/agent_link.erl
@@ -1,6 +1,6 @@
 -module(agent_link).
 
--export([add_contact/1, remove_contact/1]).
+-export([add_contact/1, remove_contact/1, get_state/0]).
 
 add_contact(Contact) ->
     Spec = {Contact,
@@ -15,4 +15,8 @@ add_contact(Contact) ->
 remove_contact(Contact) ->
     ok = supervisor:terminate_child(agent_link_sup, Contact),
     supervisor:delete_child(agent_link_sup, Contact).
+
+get_state() ->
+    link_fsm:get_state().
+
     
--- src/main/erlang/link_fsm.erl
+++ src/main/erlang/link_fsm.erl
@@ -1,12 +1,15 @@
 -module(link_fsm).
 -behavior(gen_fsm).
--export([init/1, start_link/0, terminate/3]).
+-export([init/1, get_state/0, start_link/0, handle_sync_event/4, terminate/3]).
 -export([disconnected/2, marginally_connected/2, solidly_connected/2]).
 
 -record(state, {contacts=sets:new()}).
 
 start_link() ->
     gen_fsm:start_link({local, ?MODULE}, ?MODULE, [], []).
+
+get_state() ->
+    gen_fsm:sync_send_all_state_event(?MODULE, get_state).
 
 init(_Args) ->
     {ok, disconnected, #state{}}.
@@ -55,6 +58,14 @@ solidly_connected({disconnected, _Reason, Contact}, State) ->
 	_ ->
 	    {next_state, solidly_connected, NewState}
     end.
+
+handle_sync_event(get_state, _From, StateName, State) ->
+    ConnectedStates = sets:from_list([solidly_connected, marginally_connected]),
+    CurrentState = case sets:is_element(StateName, ConnectedStates) of
+		       true -> connected;
+		       false -> disconnected
+		   end,
+    {reply, {ok, CurrentState}, StateName, State}.
 
 terminate(_Reason, _State, _Data) ->
     void.
